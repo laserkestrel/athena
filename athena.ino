bde66dbe82598d944e5739e4eed8a1e5aaa9fdef
Laser Kestrel <tidyandy@gmx.com>
2025-11-17 21:16:55 +0000

    Refactor audio setup for stereo output
    Updated audio parameters and buffer handling for stereo output. (Caused amp to not play anything)

--- athena.ino
+++ athena.ino
@@ -1,63 +1,69 @@
-#include <driver/i2s.h>
+#include <Arduino.h>
+#include "driver/i2s.h"
+#include <math.h>
 
 // I2S pins
-#define I2S_WS   25  // LRC
-#define I2S_BCLK 26  // BCLK
-#define I2S_DOUT 27  // DIN
+#define I2S_WS   25   // LRC
+#define I2S_BCLK 26   // BCLK
+#define I2S_DOUT 27   // DIN
+#define SD_PIN   13   // MAX98357A shutdown (active HIGH)
 
 // LED pin
-#define LED_PIN 2    // On-board LED (adjust if external)
+#define LED_PIN 2     // On-board LED
 
-#define SAMPLE_RATE 16000
-#define AMPLITUDE 1000
-#define TONE_FREQ 440  // Test tone (Hz)
-#define BUFFER_SIZE 512
+// Audio parameters
+#define SAMPLE_RATE 16000   // 16 kHz sample rate
+#define AMPLITUDE 1000      // Sine wave amplitude
+#define TONE_FREQ 440       // Frequency of sine wave (Hz)
+#define BUFFER_SIZE 512     // Samples per buffer
 
-int16_t sampleBuffer[BUFFER_SIZE];
+int16_t sampleBuffer[BUFFER_SIZE * 2]; // Stereo interleaved buffer
 
 void setup() {
   Serial.begin(115200);
 
-  // Enable amplifier
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  // Enable MAX98357A
+  pinMode(SD_PIN, OUTPUT);
+  digitalWrite(SD_PIN, HIGH);  // enable amplifier
 
   // Setup LED pin
   pinMode(LED_PIN, OUTPUT);
 
   // Setup I2S
   i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-    .sample_rate = SAMPLE_RATE,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-    .communication_format = I2S_COMM_FORMAT_I2S,
-    .intr_alloc_flags = 0,
-    .dma_buf_count = 4,
-    .dma_buf_len = 512,
-    .use_apll = false
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+      .sample_rate = SAMPLE_RATE,
+      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = I2S_COMM_FORMAT_I2S,
+      .intr_alloc_flags = 0,
+      .dma_buf_count = 4,
+      .dma_buf_len = 512,
+      .use_apll = false
   };
 
   i2s_pin_config_t pin_config = {
-    .bck_io_num = I2S_BCLK,
-    .ws_io_num = I2S_WS,
-    .data_out_num = I2S_DOUT,
-    .data_in_num = I2S_PIN_NO_CHANGE
+      .bck_io_num = I2S_BCLK,
+      .ws_io_num = I2S_WS,
+      .data_out_num = I2S_DOUT,
+      .data_in_num = I2S_PIN_NO_CHANGE
   };
 
   i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
   i2s_set_pin(I2S_NUM_0, &pin_config);
+
+  // Precompute sine wave buffer (stereo interleaved)
+  for (int i = 0; i < BUFFER_SIZE; i++) {
+    int16_t sample = AMPLITUDE * sin(2 * PI * TONE_FREQ * i / SAMPLE_RATE);
+    sampleBuffer[i * 2]     = sample;  // left
+    sampleBuffer[i * 2 + 1] = sample;  // right
+  }
 }
 
 void loop() {
-  // Generate a short sine wave buffer
-  for (int i = 0; i < BUFFER_SIZE; i++) {
-    sampleBuffer[i] = AMPLITUDE * sin(2 * PI * TONE_FREQ * i / SAMPLE_RATE);
-  }
-
-  // Send buffer to MAX98357A via I2S
+  // Send sine wave buffer via I2S
   size_t bytesWritten;
-  i2s_write(I2S_NUM_0, sampleBuffer, BUFFER_SIZE * sizeof(int16_t), &bytesWritten, portMAX_DELAY);
+  i2s_write(I2S_NUM_0, sampleBuffer, sizeof(sampleBuffer), &bytesWritten, portMAX_DELAY);
 
   // LED breathing effect
   for (int i = 0; i <= 255; i++) {      // Brighten
